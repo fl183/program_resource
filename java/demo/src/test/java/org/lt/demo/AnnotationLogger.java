200b2a17c5605168d80ec8f1a5e76dd7c50ce3d8
fengluo <1104219819@qq.com>
2018-12-16 14:11:57 +0800

    大海-demo

---
+++ java/demo/src/test/java/org/lt/demo/AnnotationLogger.java
@@ -0,0 +1,19 @@
+package org.lt.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class AnnotationLogger {
+    @Test
+    public void test2 (){
+        log.debug("debug");
+        log.info("info");
+        log.error("error");
+    }
+}
